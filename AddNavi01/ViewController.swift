cf2bb2b9ad05a0c4825e45f835ec52782332f418
Fujio Nakatsuka <com.nifty.cbm01799>
2020-01-11 14:40:33 +0900

    Initial Commit

---
+++ AddNavi01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AddNavi01
+//
+//  Created by 中塚富士雄 on 2020/01/11.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
